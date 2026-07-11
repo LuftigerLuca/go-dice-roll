f0340c50225bc4cf450dfb004293ed44a3ade77b
Luca Schumacher <l.schumacher@luftiger.eu>
2026-07-11 22:48:23 +0200

    feat(http): migrate to net/http and enhance roll response structure

--- main.go
+++ main.go
@@ -1,42 +1,88 @@
 package main
 
 import (
+	"encoding/json"
+	"log/slog"
 	"math"
 	"math/rand"
+	"net/http"
 	"strconv"
-
-	"github.com/gin-gonic/gin"
+	"time"
 )
 
-func main() {
-	router := gin.Default()
-	router.GET("/roll", getRoll)
-
-	router.Run("localhost:8080")
+type ErrorResponse struct {
+	Error string `json:"error"`
 }
 
-func getRoll(c *gin.Context) {
-	sidesStr := c.Query("sides")
-	sides, err := strconv.Atoi(sidesStr)
+type RollResponse struct {
+	Result      int     `json:"result"`
+	Sides       int     `json:"sides"`
+	Average     float64 `json:"average"`
+	Probability float64 `json:"probability"`
+}
 
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("GET /roll/{sides}", getRoll)
+
+	addr := ":8080"
+	slog.Info("starting the webserver", "address", addr)
+	err := http.ListenAndServe(addr, middleware(mux))
 	if err != nil {
-		c.JSON(400, gin.H{"message": "Sides must be a number!"})
+		slog.Error("error while starting the webserver!", "error", err)
+	}
+}
+
+func getRoll(w http.ResponseWriter, r *http.Request) {
+	sidesStr := r.PathValue("sides")
+	sides, err := strconv.Atoi(sidesStr)
+	if err != nil {
+		res := ErrorResponse{
+			Error: err.Error(),
+		}
+		writeResponse(http.StatusBadRequest, w, res)
 		return
 	}
 
 	if sides <= 0 {
-		c.JSON(400, gin.H{"message": "Sides must not be zero or lower!"})
+		res := ErrorResponse{
+			Error: "sides must be a number!",
+		}
+		writeResponse(http.StatusBadRequest, w, res)
 		return
 	}
 
 	result := rand.Intn(sides) + 1
-	average := float64(1+sides) / 2
-	probability := math.Round((1.0/float64(sides))*100) / 100
+	res := RollResponse{
+		Result:      result,
+		Sides:       sides,
+		Average:     float64(1+sides) / 2,
+		Probability: math.Round((1.0/float64(sides))*100) / 100,
+	}
+	writeResponse(http.StatusOK, w, res)
+}
 
-	c.IndentedJSON(200, gin.H{
-		"result":      result,
-		"sides":       sides,
-		"average":     average,
-		"probability": probability,
+func writeResponse(code int, writer http.ResponseWriter, response any) {
+	writer.WriteHeader(code)
+	if err := json.NewEncoder(writer).Encode(response); err != nil {
+		slog.Warn("an error occurred!", "error", err)
+	}
+}
+
+func middleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		defer func() {
+			err := recover()
+			if err != nil {
+				slog.Warn("web middleware recovery caught something!", "error", err)
+				http.Error(w, "There was an internal server error", http.StatusInternalServerError)
+			}
+		}()
+
+		s := time.Now()
+		next.ServeHTTP(w, r)
+		d := time.Since(s)
+		slog.Info(r.Method, "path", r.URL, "duration", d)
 	})
 }
